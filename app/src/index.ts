8af6ac67bba733ec3c98d05977b8b8c2fdad9711
Ricardo Seromenho <ricardo.seromenho@gmail.com>
2022-07-19 10:20:23 +0100

    Add dev tools lint/prettier to the app

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,13 +1,14 @@
 import algosdk from 'algosdk'
 
-const token = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
-const server = 'http://127.0.0.1';
-const port = 4001;
-const client = new algosdk.Algodv2(token, server, port);
+const token = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+const server = 'http://127.0.0.1'
+const port = 4001
+const client = new algosdk.Algodv2(token, server, port)
 
-(async () => {
-  console.log('Status: ');
+;(async () => {
+  console.log('Status: ')
   console.log(await client.status().do())
 })().catch((e) => {
-  console.log(e);
-});
+  console.log(e)
+})
+const b = {g: 9}

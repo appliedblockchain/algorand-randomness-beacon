a093ccdf0803bc37ca678c384df36d15458b4624
Ricardo Seromenho <ricardo.seromenho@gmail.com>
2022-07-26 12:07:00 +0100

    Update main flow with the methods getting real data

--- app/src/loop.ts
+++ app/src/loop.ts
@@ -1,23 +1,48 @@
-import { randomVrfInput } from './utils/build-vrf-input'
-import { generateVrfKeyPair, vrfProofToHash, vrfProve } from './utils/libsodium-wrapper'
+import { getBlockSeed, getLastRound } from './utils/algo-utils'
+import { vrfProofToHash, vrfProve } from './utils/libsodium-wrapper'
+import { getOracleKeyPair } from './utils/oracle-key-pair'
+import buildVrfInput from './utils/vrf'
+import logger from './logger'
+import { BLOCK_INTERVAL } from './constants'
 
-const oracleKeyPair = generateVrfKeyPair()
+const getProofHash = async (vrfInput: string): Promise<string> => {
+  const oracleKeyPair = await getOracleKeyPair()
+  const proof = vrfProve(oracleKeyPair.sk, Buffer.from(vrfInput))
+  const hash = vrfProofToHash(proof)
 
-const BLOCK_AVERAGE = 300 // ms
+  return hash.toString('hex')
+}
 
-const loop = () => {
-  let blockNumber = 0
-  setInterval(() => {
-    blockNumber++
-    console.log({ blockNumber })
-    if (blockNumber % 8 !== 0) {
+const mainFlow = async () => {
+  try {
+    logger.info('Getting last round')
+    const lastRound = await getLastRound()
+    if (!lastRound) {
+      throw new Error("can't get last round")
+    }
+    if (lastRound % BLOCK_INTERVAL !== 0) {
+      logger.info('Ignoring last round', { lastRound })
       return
     }
-    const vrfInput = randomVrfInput(blockNumber)
-    const proof = vrfProve(oracleKeyPair.sk, Buffer.from(vrfInput))
-    const hash = vrfProofToHash(proof)
-    console.log({ proof: proof.toString('hex'), hash: hash.toString('hex'), blockNumber })
-  }, BLOCK_AVERAGE)
+
+    logger.info(`Getting block seed for ${lastRound}`)
+    const blockSeed = await getBlockSeed(lastRound)
+    if (!blockSeed) {
+      throw new Error("can't get last round block seed")
+    }
+
+    logger.info('Building VRF input', { lastRound, blockSeed })
+    const vrfInput = buildVrfInput(lastRound, blockSeed)
+    logger.info('Getting the proof hash', { vrfInput })
+    const hash = await getProofHash(vrfInput)
+    logger.debug({ lastRound, blockSeed, vrfInput, hash })
+  } catch (error) {
+    logger.error(error)
+  }
+}
+
+const loop = async () => {
+  setInterval(mainFlow, 1000)
 }
 
 export default loop

5da68f99930f4f708a5533b9b05f395ef8b5d981
Ricardo Seromenho <ricardo.seromenho@gmail.com>
2022-07-26 15:59:59 +0100

    Add algo method to get a value from the app state

--- app/src/utils/algo-utils.ts
+++ app/src/utils/algo-utils.ts
@@ -1,4 +1,5 @@
 import algosdk from 'algosdk'
+import { TealKeyValue } from 'algosdk/dist/types/src/client/v2/algod/models/types'
 
 const client = new algosdk.Algodv2(process.env.ALGOD_TOKEN, process.env.ALGOD_SERVER, process.env.ALGOD_PORT)
 
@@ -10,4 +11,29 @@ export const getLastRound = async (): Promise<number> => {
 export const getBlockSeed = async (round: number): Promise<string> => {
   const block = await client.block(round).do()
   return block?.block?.seed?.toString('hex')
+}
+
+export const getGlobalState = async (): Promise<TealKeyValue[]> => {
+  const applicationInfoResponse = await client
+    .accountApplicationInformation(process.env.APP_CREATOR_ADDRESS, Number(process.env.APP_ID))
+    .do()
+  return applicationInfoResponse['created-app']['global-state']
+}
+
+export const getValueFromKeyValue = (kv: TealKeyValue, decode = true) => {
+  return {
+    [1]: decode ? Buffer.from(kv.value.bytes, 'base64').toString() : Buffer.from(kv.value.bytes, 'base64'),
+    [2]: kv.value.uint,
+  }[Number(kv.value.type)]
+}
+
+export const getGlobalStateValue = async (key: string): Promise<string | number | bigint | Buffer> => {
+  const globalState = await getGlobalState()
+  const base64Key = Buffer.from(key).toString('base64')
+  const keyValue: TealKeyValue = globalState.find((kv) => kv.key === base64Key)
+  if (!keyValue) {
+    throw new Error('Key not found')
+  }
+
+  return getValueFromKeyValue(keyValue)
 }

b5a0f6dad3b655d8d3c15d75f57bb631dd25d245
Ricardo Seromenho <ricardo.seromenho@gmail.com>
2022-07-29 14:09:39 +0100

    Add kms util to get the VRF key

---
+++ services/vrf/src/utils/kms.ts
@@ -0,0 +1,33 @@
+import * as AWS from '@aws-sdk/client-kms'
+
+const credentials = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+}
+
+const LOCALSTACK_ENVIRONMENTS = ['development', 'local', 'test']
+const useLocalstack = LOCALSTACK_ENVIRONMENTS.includes(process.env.NODE_ENV)
+
+export const client = new AWS.KMS({
+  credentials,
+  region: process.env.AWS_REGION,
+  endpoint: useLocalstack ? 'http://localhost:4566' : undefined,
+})
+
+// Cache the key to avoid keep doing decrypt
+let decryptedKey: string
+const decryptVrfKey = async () => {
+  if (decryptedKey) {
+    return decryptedKey
+  }
+
+  const { Plaintext } = await client.decrypt({
+    KeyId: process.env.AWS_KMS_KEY_ID,
+    CiphertextBlob: Buffer.from(process.env.VRF_ENCRYPTED_SECRET_KEY, 'base64'),
+  })
+
+  decryptedKey = Buffer.from(Plaintext).toString('utf-8')
+  return decryptedKey
+}
+
+export default decryptVrfKey

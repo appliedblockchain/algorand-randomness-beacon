f9da1041496a839a09162e6e178164ad3cd00e91
Ricardo Seromenho <ricardo.seromenho@gmail.com>
2022-08-01 10:28:57 +0100

    add gRPC server on VRF service

--- services/vrf/src/index.ts
+++ services/vrf/src/index.ts
@@ -1,5 +1,28 @@
 import 'dotenv/config'
+import * as grpc from '@grpc/grpc-js'
+import * as protoLoader from '@grpc/proto-loader'
+import path from 'path'
+import { ProtoGrpcType } from './proto/vrf'
 
-setInterval(() => {
-  console.log('TODO: GRPC server')
-}, 10000)
+const PROTO_PATH = path.join(__dirname, './proto/vrf.proto')
+
+import logger from './logger'
+import handlers from './server'
+
+const grpcAddress = `${process.env.GRPC_ADDRESS}:${process.env.GRPC_PORT}`
+
+const startServer = () => {
+  const packageDefinition = protoLoader.loadSync(PROTO_PATH)
+  const proto = grpc.loadPackageDefinition(packageDefinition) as unknown as ProtoGrpcType
+  const server = new grpc.Server()
+  server.addService(proto.vrf.Vrf.service, handlers)
+  server.bindAsync(grpcAddress, grpc.ServerCredentials.createInsecure(), (error) => {
+    if (error) {
+      throw error
+    }
+    server.start()
+    logger.info(`gRPC listening on ${grpcAddress}`)
+  })
+}
+
+startServer()

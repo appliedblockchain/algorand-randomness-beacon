3be2968f2610c48eb6bf605d2cc17e009bc13b03
Bruno Neves <bruno.neves@appliedblockchain.com>
2022-08-04 22:42:29 +0100

    Manifests

---
+++ stack-eks/deploy-app-env-vars.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Generating temporary manifest with secrets."
+cp app-env-vars.secrets.txt app-env-vars.secrets-tmp.yaml
+sed -i "s^{{ALGOD_TOKEN}}^$ALGOD_TOKEN^g" app-env-vars.secrets-tmp.yaml
+sed -i "s^{{SERVICE_KEY}}^$SERVICE_KEY^g" app-env-vars.secrets-tmp.yaml
+sed -i "s^{{AWS_SECRET_ACCESS_KEY}}^$AWS_SECRET_ACCESS_KEY^g" app-env-vars.secrets-tmp.yaml
+sed -i "s^{{VRF_SECRET_KEY}}^$VRF_SECRET_KEY^g" app-env-vars.secrets-tmp.yaml
+
+echo "Applying secrets"
+
+kubectl apply -f app-env-vars.secrets-tmp.yaml
+
+if [ $? -eq 0 ]; then
+    echo "Secrets applied with success."
+else
+    echo "Error Applying secrets."
+    exit 1
+fi
+
+echo "Deleting temporary manifest with secrets."
+rm -fr  app-env-vars.secrets-tmp.yaml
+
+if [ $? -eq 0 ]; then
+    echo "Secrets applied with success."
+else
+    echo "Error Applying secrets."
+    exit 1
+fi

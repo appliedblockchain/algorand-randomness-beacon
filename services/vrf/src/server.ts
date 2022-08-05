9be7d0a8b2c5d0d8063efbe1d9d00ca3e36d5876
Nilson Bertola <nilson@appliedblockchain.com>
2022-08-05 11:24:09 +0100

    add trace id to logger

--- services/vrf/src/server.ts
+++ services/vrf/src/server.ts
@@ -1,6 +1,7 @@
 import * as grpc from '@grpc/grpc-js'
 import * as Sentry from '@sentry/node'
-import logger from './logger'
+import { randomUUID } from 'crypto'
+import parentLogger from './logger'
 import { VrfHandlers } from './proto/vrf/Vrf'
 import { VRFInput } from './proto/vrf/VRFInput'
 import { VRFProof } from './proto/vrf/VRFProof'
@@ -9,6 +10,7 @@ import { vrfProve } from './utils/libsodium-wrapper'
 
 const server: VrfHandlers = {
   GenerateProof: async (call: grpc.ServerUnaryCall<VRFInput, VRFProof>, callback: grpc.sendUnaryData<VRFProof>) => {
+    const logger = parentLogger.child({ traceId: randomUUID() })
     const vrfInputString = Buffer.from(call.request.vrfInput).toString()
     try {
       logger.info('Generate proof handler', { vrfInputString })

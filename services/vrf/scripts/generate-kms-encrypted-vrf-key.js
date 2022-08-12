1ee7149b06c188fe83b533e77342146cef51c55c
Ricardo Seromenho <ricardo.seromenho@gmail.com>
2022-08-12 16:38:55 +0100

    add script to generate vrf key pair and store it on kms

---
+++ services/vrf/scripts/generate-kms-encrypted-vrf-key.js
@@ -0,0 +1,24 @@
+const AWS = require('@aws-sdk/client-kms')
+const sodium = require('@appliedblockchain/sodium-native-vrf')
+const pk = Buffer.alloc(sodium.crypto_vrf_PUBLICKEYBYTES)
+const sk = Buffer.alloc(sodium.crypto_vrf_SECRETKEYBYTES)
+sodium.crypto_vrf_keypair(pk, sk)
+
+const credentials = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+}
+
+const client = new AWS.KMS({
+  credentials,
+  region: process.env.AWS_REGION,
+})
+
+client
+  .encrypt({
+    Plaintext: pk,
+    KeyId: process.env.AWS_KMS_KEY_ID,
+  })
+  .then((result) => {
+    console.log(Buffer.from(result.CiphertextBlob).toString('base64'))
+  })
